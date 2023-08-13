84dec821cb6d908c87b8462be23356b606ddec2e
Stefan Armbruster <stefan@armbruster-it.de>
2023-08-13 14:06:56 +0200

    test: test against csv file

---
+++ cypher_direction/cypher_direction.py
@@ -0,0 +1,2 @@
+def fix_direction(query: str, schema: str):
+    return query
--- tests/test_cypher_direction.py
+++ tests/test_cypher_direction.py
@@ -1,8 +1,8 @@
 import pytest
+from cypher_direction import cypher_direction
 
 from pytest_csv_params.decorator import csv_params
 
 @csv_params("tests/resources/examples.csv")
 def test_statements(statement: str, schema: str, correct_query:str):
-    print(schema)
-    print(correct_query)
+    assert correct_query == cypher_direction.fix_direction(statement, schema)
